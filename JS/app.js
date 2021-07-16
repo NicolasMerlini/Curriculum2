205d308c35b908364ff1bf0415e9ffd16bbe9f5a
NicolasMerlini <nicolas_merlini@hotmail.com>
2021-07-16 11:50:23 -0300

    boton descargar CV

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,2 @@
-  let name = prompt('¿Cuál es tu nombre?');
-  alert('¡Bienvenido/a ' + name + '!, a continuación vas a ver el Curriculum Vitae de Nicolás Merlini');
+/*   let name = prompt('¿Cuál es tu nombre?');
+  alert('¡Bienvenido/a ' + name + '!, a continuación vas a ver el Curriculum Vitae de Nicolás Merlini'); */
